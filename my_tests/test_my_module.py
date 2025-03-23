3937a688c05ccfffa793bd9e0deb1ae80764d838
wang <QMxgfy5285016(!>
2025-03-23 21:56:57 +0100

    first commit

---
+++ my_tests/test_my_module.py
@@ -0,0 +1,12 @@
+def test_print():
+    print('This is test.')
+    # assert True
+
+
+# class TestMath:
+#     def test_add(self):
+#         assert 1 + 1 == 2
+#
+#
+# def test_something():
+#     assert 1 + 1 == 3
